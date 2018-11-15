132ef05645029917f6423b127f9c1ea8bd0ac0d1
Dmitry V <exrump@gmail.com>
2018-11-14 21:41:30 -0500

    Init

--- app.js
+++ app.js
@@ -1,7 +1,24 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
+var config = require('./config');
+var Client = require('mariasql');
 
-app.get('/', (req, res) => res.send('Hello World!'))
+var c = new Client({
+    host: '127.0.0.1',
+    user: 'root',
+    password: config.mariaPassword,
+    port: 3306
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+c.query('SHOW DATABASES', function (err, rows) {
+    if (err)
+        throw err;
+    console.dir(rows);
+});
+
+c.end();
+
+app.get('/', (req, res) => res.send('Hello World!'));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));

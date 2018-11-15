98f801c2243da1963615b218e1c5d6f9bf26197f
Dmitry Vereykin <exrump@gmail.com>
2018-11-15 12:47:02 -0500

    DB Settings

--- app.js
+++ app.js
@@ -1,23 +1,22 @@
 const express = require('express');
 const app = express();
 const port = 3000;
-var config = require('./config');
+require('dotenv').config();
 var Client = require('mariasql');
 
-var c = new Client({
-    host: '127.0.0.1',
-    user: 'root',
-    password: config.mariaPassword,
-    port: 3306
+var connection = new Client({
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS
 });
 
-c.query('SHOW DATABASES', function (err, rows) {
+connection.query('SHOW DATABASES', function (err, rows) {
     if (err)
         throw err;
     console.dir(rows);
 });
 
-c.end();
+connection.end();
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
